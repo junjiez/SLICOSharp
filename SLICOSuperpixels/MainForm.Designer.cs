9711f0d705d89b0180b1fb6c6f16688d99f74a00
DailyC <junjiezhung@gmail.com>
2016-12-02 15:16:46 -0500

    Initial commit
    Commit the project

---
+++ SLICOSuperpixels/MainForm.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace SLICOSuperpixelsCSharp
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button_ChangeImg = new System.Windows.Forms.Button();
+            this.button_SLICO = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
+            this.splitContainer2.Panel1.SuspendLayout();
+            this.splitContainer2.Panel2.SuspendLayout();
+            this.splitContainer2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.splitContainer2);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.textBox1);
+            this.splitContainer1.Panel2.Controls.Add(this.groupBox1);
+            this.splitContainer1.Size = new System.Drawing.Size(857, 476);
+            this.splitContainer1.SplitterDistance = 635;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // splitContainer2
+            // 
+            this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer2.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer2.Name = "splitContainer2";
+            // 
+            // splitContainer2.Panel1
+            // 
+            this.splitContainer2.Panel1.Controls.Add(this.pictureBox1);
+            // 
+            // splitContainer2.Panel2
+            // 
+            this.splitContainer2.Panel2.Controls.Add(this.pictureBox2);
+            this.splitContainer2.Size = new System.Drawing.Size(635, 476);
+            this.splitContainer2.SplitterDistance = 320;
+            this.splitContainer2.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(320, 476);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox2.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(311, 476);
+            this.pictureBox2.TabIndex = 0;
+            this.pictureBox2.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(3, 89);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(205, 383);
+            this.textBox1.TabIndex = 5;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Controls.Add(this.button_ChangeImg);
+            this.groupBox1.Controls.Add(this.button_SLICO);
+            this.groupBox1.Location = new System.Drawing.Point(3, 3);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 80);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Number of Superpixels:";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "100",
+            "200",
+            "300",
+            "400",
+            "500",
+            "600",
+            "700",
+            "800",
+            "900",
+            "1000"});
+            this.comboBox1.Location = new System.Drawing.Point(7, 20);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(187, 21);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // button_ChangeImg
+            // 
+            this.button_ChangeImg.Location = new System.Drawing.Point(94, 47);
+            this.button_ChangeImg.Name = "button_ChangeImg";
+            this.button_ChangeImg.Size = new System.Drawing.Size(100, 25);
+            this.button_ChangeImg.TabIndex = 1;
+            this.button_ChangeImg.Text = "Change Image";
+            this.button_ChangeImg.UseVisualStyleBackColor = true;
+            this.button_ChangeImg.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button_SLICO
+            // 
+            this.button_SLICO.Location = new System.Drawing.Point(7, 47);
+            this.button_SLICO.Name = "button_SLICO";
+            this.button_SLICO.Size = new System.Drawing.Size(80, 25);
+            this.button_SLICO.TabIndex = 3;
+            this.button_SLICO.Text = "SLICO";
+            this.button_SLICO.UseVisualStyleBackColor = true;
+            this.button_SLICO.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(857, 476);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "SLICOSuperpixels";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.splitContainer2.Panel1.ResumeLayout(false);
+            this.splitContainer2.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
+            this.splitContainer2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.SplitContainer splitContainer2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button button_ChangeImg;
+        private System.Windows.Forms.Button button_SLICO;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ SLICOSuperpixels/MainForm.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using SLICOSuperpixelsCSharp.Properties;
+
+namespace SLICOSuperpixelsCSharp
+{
+    public partial class MainForm : Form
+    {
+        
+        // scource image and superpixel segmented image
+        //
+        private Bitmap imgBitmap = Resources.IR_Building;
+        private Bitmap imgSuperpixel = null;
+
+        // SLICO related members
+        //
+        private SLICO _slico = new SLICO();     // SLICO 
+        private int[] _klabels;                  // SLICO segment label vector for all pixels
+        private int _numlabels;                  // number of resulted labels
+        private int K;                          // user specified segment numbers
+
+        // Flag to control the selection of images
+        //
+        private int _picFlag = 0;
+        private int NUM_PIC = 3;                // number of pictures for demonstration purpose
+
+        // Propertie to control the image selection
+        //
+        private int MpicFlag
+        {
+            get
+            {
+                _picFlag = _picFlag%NUM_PIC;
+                return _picFlag;
+            }
+        }
+        private Bitmap BitmapSelected
+        {
+            get
+            {
+                switch (MpicFlag)
+                {
+                    case 0:
+                        return Resources.IR_Building;
+                    case 1:
+                        return Resources.MRI_Prostate;
+                    case 2:
+                        return Resources.MRI_Chest_Axial;
+                    default:
+                        return Resources.MRI_Prostate;
+                }
+            }
+        }
+
+        private String StrBitmapName
+        {
+            get
+            {
+                switch (MpicFlag)
+                {
+                    case 0:
+                        return "Thermal Image - Building ";
+                    case 1:
+                        return "MRI Image - Prostate";
+                    case 2:
+                        return "MRI Image - Chest Axial";
+                    default:
+                        return "MRI Image - Prostate";
+                }
+            }
+        }
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // initialize controls
+            splitContainer1.SplitterDistance = splitContainer1.Width - 218;
+            splitContainer2.SplitterDistance = (int) (splitContainer2.Width*0.5);
+
+            comboBox1.SelectedIndex = 9;
+
+            // PictureBox
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.Image = imgBitmap;
+
+            pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+
+            textBox1.Text = Resources.strProgramInfo + Resources.strHelpInfo;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            _picFlag ++;
+
+            imgBitmap = this.BitmapSelected;
+            pictureBox1.Image = imgBitmap;
+            pictureBox2.Image = null;
+
+            textBox1.Text += (Resources.strImageReload + "\r\n");
+            textBox1.Text += (StrBitmapName + "\r\n");
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (null == comboBox1.SelectedText)
+            {
+                textBox1.Text += @"\nSelect Superpixel Number First!\n";
+                return;
+            }
+            else
+            {
+                K = int.Parse(comboBox1.SelectedItem.ToString());
+            }
+            if (null != imgBitmap)
+            {
+                // Perform SLIO to input image
+                imgSuperpixel = _slico.PerformSLICO_ForGivenK(ref imgBitmap, out _klabels, out _numlabels, K, Color.Red, 10);
+            }
+
+            // update Pixturebox2
+            pictureBox2.Image = imgSuperpixel;
+
+            textBox1.Text += (Resources.strSegmentResult + _numlabels.ToString() + "\r\n");
+        }
+    }
+}
